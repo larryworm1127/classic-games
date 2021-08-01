288c1c52a9409ec531c486823c0f8d8a82d6643d
Le Rong Shi <larry.shi@mail.utoronto.ca>
2021-08-01 15:40:19 -0400

    added home page

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('@modules/home/home.module').then((m) => m.HomeModule)
+  },
+  {
+    path: 'ttt',
+    loadChildren: () => import('@modules/tic-tac-toe/tic-tac-toe.module').then((m) => m.TicTacToeModule)
+  },
+  {
+    path: 'sudoku',
+    loadChildren: () => import('@modules/sudoku/sudoku.module').then((m) => m.SudokuModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppRoutingModule } from "./app-routing.module";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    AppRoutingModule,
+    NgbModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
