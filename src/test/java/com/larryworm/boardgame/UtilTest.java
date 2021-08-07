a4f8bc09b886b29564d2d0be0faf882094e3266e
Le Rong Shi <larry.shi@mail.utoronto.ca>
2021-08-07 11:48:08 -0400

    minor change to util

--- src/test/java/com/larryworm/boardgame/UtilTest.java
+++ src/test/java/com/larryworm/boardgame/UtilTest.java
@@ -14,4 +14,18 @@ class UtilTest {
         List<Integer> expected = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
         assertEquals(expected, Util.flatten2dArray(array), "2d Integer array not flattened correctly");
     }
+
+    @Test
+    void flatten2dList() {
+        var list = List.of(List.of(1, 2, 3), List.of(4, 5, 6), List.of(7, 8, 9));
+        List<Integer> expected = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        assertEquals(expected, Util.flatten2dList(list), "2d Integer list not flattened correctly");
+    }
+
+    @Test
+    void twoDArrayTo2dList() {
+        Integer[][] array = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        var expected = List.of(List.of(1, 2, 3), List.of(4, 5, 6), List.of(7, 8, 9));
+        assertEquals(expected, Util.twoDArrayTo2dList(array), "2d Integer array not converted to list correctly");
+    }
 }
