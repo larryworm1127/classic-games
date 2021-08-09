caf478705eba1876ab19cf1bfdd885dd77db6514
Le Rong Shi <larry.shi@mail.utoronto.ca>
2021-08-08 20:16:30 -0400

    renamed

--- src/main/java/com/larryworm/classicgames/ClassicGamesApplication.java
+++ src/main/java/com/larryworm/classicgames/ClassicGamesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BoardGameApplication {
+public class ClassicGamesApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BoardGameApplication.class, args);
+        SpringApplication.run(ClassicGamesApplication.class, args);
     }
 
 }
