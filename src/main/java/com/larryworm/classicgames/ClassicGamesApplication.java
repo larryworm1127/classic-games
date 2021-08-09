482d746ff4ffb8be53510a7a046d1da0fce82d6a
Le Rong Shi <larry.shi@mail.utoronto.ca>
2021-08-08 22:24:58 -0400

    fixed more config

--- src/main/java/com/larryworm/classicgames/ClassicGamesApplication.java
+++ src/main/java/com/larryworm/classicgames/ClassicGamesApplication.java
@@ -2,12 +2,20 @@ package com.larryworm.classicgames;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @SpringBootApplication
+@Controller
 public class ClassicGamesApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ClassicGamesApplication.class, args);
     }
 
+    @GetMapping(value = "/{path:[^.]*}")
+    public String redirect(@PathVariable String path) {
+        return "forward:/";
+    }
 }
